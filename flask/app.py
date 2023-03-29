c70e176f492079f8dbb2076ddb66c0721daf680b
Jakab Márton Zoltán <x5mc0i@inf.elte.hu>
2023-03-29 10:00:55 +0200

    touched up UI

--- flask/app.py
+++ flask/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, session, json
 from flask_session import Session
 
@@ -5,6 +6,7 @@ import base64
 from io import BytesIO
 from matplotlib.figure import Figure
 import numpy as np
+from matplotlib import use
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
@@ -14,15 +16,18 @@ import sys
 
 # append the path of the
 # parent directory
-import logging;
+import logging
 sys.path.append("..")
- 
+from trueskill import Rating, rate, TrueSkill
+logging.basicConfig(level=logging.INFO)
+
 # import method from sibling
 # module
-from trueskill import Rating, rate
- 
-#TODO: Refactor with new data structure
-#TODO: Finish swap, swapall and remove functions
+
+# TODO: Refactor with new data structure
+# TODO: Finish swap, swapall and remove functions
+
+use('agg')  # MATPLOTLIB IS NOT THREAD SAFE.
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
@@ -30,78 +35,70 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 @app.route('/')
+@app.route('/main')
 def index():
-    
+
+    print("MI A FASZ")
     session.update({
-        'teams' : {
-            'team1' : {},
-            'team2' : {}
+        'teams': {
+            'team1': {},
+            'team2': {}
         }
-        
+
     })
-    session.pop('img','')
-    
-    
-    
-    #print(session['teams'])
+    session.pop('img', '')
+
+    # print(session['teams'])
     return render_template('main.html')
 
 
-@app.route('/add_player', methods = ['POST', 'GET'])
+@app.route('/add_player', methods=['POST', 'GET'])
 def add_to_list():
     if request.method == 'POST':
         formitem = request.form
         print('got from form: ', formitem)
 
-
-
-
-        s=dict(session['teams'].items())
+        s = dict(session['teams'].items())
         team = formitem['team']
         playerName = formitem['playerName']
-        s[team][playerName] = {'mu' : '', 'sigma' : ''}
-
+        s[team][playerName] = {'mu': '', 'sigma': ''}
 
         s[team][playerName]['mu'] = formitem['mu']
         s[team][playerName]['sigma'] = formitem['sigma']
 
-
-
         return render_template('main.html')
 
-@app.route('/main', methods = ['GET'])
-def main():
-    return render_template('main.html')
 
-@app.route('/manage', methods = ['GET'])
+
+@app.route('/manage', methods=['GET','POST'])
 def manage():
+    logging.info(request.form)
     return render_template('manage.html')
 
-@app.route('/calculate', methods = ['POST'])
+
+@app.route('/calculate', methods=['POST'])
 def calculate():
     s = dict(session['teams'].items())
-    print('CALCULATING: ' , s)
-   
+    print('CALCULATING: ', s)
+
     ratings = []
-    for teamName, teamMembers in s.items(): 
+    for teamName, teamMembers in s.items():
         playerRating = []
-        
+
         for member, rating in teamMembers.items():
-            
-            playerRating.append(Rating(mu=float(rating['mu']), sigma=float(rating['sigma'])))
-        
+
+            playerRating.append(
+                Rating(mu=float(rating['mu']), sigma=float(rating['sigma'])))
 
         ratings.append(playerRating)
 
-    
-
-    
     rated = rate(ratings)
-    
-    rated_flat = [item for sublist in rated for item in sublist] #flatten
 
-    #dict items are ordered since python 3.6!
+    rated_flat = [item for sublist in rated for item in sublist]  # flatten
+
+    # dict items are ordered since python 3.6!
 
     i = 0
     for teams, teamMembers in s.items():
@@ -109,73 +106,62 @@ def calculate():
 
             rating['mu'] = rated_flat[i].mu
             rating['sigma'] = rated_flat[i].sigma
-            i+=1
-    
+            i += 1
 
-    #make figure
+    # make figure
+    # MAY BE UNSAFE
     plt.clf()
-    session.pop('img','')
-    x_axis = np.arange(0,50,0.01)
+    session.pop('img', '')
+    x_axis = np.arange(0, 50, 0.01)
     for teanName, teamMember in s.items():
         for teamMember, values in teamMember.items():
-            #invent some clever colors here
-            plt.plot(x_axis, norm.pdf(x_axis, values['mu'], values['sigma']))
-    
-    
-    
-   
-    
+            # invent some clever colors here
+            plt.plot(x_axis, norm.pdf(
+                x_axis, values['mu'], values['sigma']), scalex=1.5, animated=True)
 
     # Save it to a temporary buffer.
     buf = BytesIO()
-    plt.savefig(buf, format="png", )
+    plt.savefig(buf, format="png")
     # Embed the result in the html output.
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     session['img'] = data
 
-    
     return render_template('main.html')
 
-@app.route('/remove_player', methods = ['POST'])
+
+@app.route('/remove_player', methods=['POST'])
 def remove():
     s = dict(session['teams'])
-    
+
     print(s)
-    #form = dict(request.form)['playerName'].split('\'')[1] #unholy, find a better solution
-    
+    # form = dict(request.form)['playerName'].split('\'')[1] #unholy, find a better solution
+
     for teams, teamMembers in s.items():
-        teamMembers.pop(request.form['playerName'], '') 
-    
-    
-   
-    
+        teamMembers.pop(request.form['playerName'], '')
+
     return render_template('main.html')
 
-@app.route('/remove_team', methods = ['POST'])
+
+@app.route('/remove_team', methods=['POST'])
 def remove_team():
-    
-    session['teams'].pop(request.form['team'])
-   
-    print(session)
-    
 
+    session['teams'].pop(request.form['team'])
+
+    print(session)
 
     return render_template('main.html')
 
 
-@app.route('/add_team', methods = ['POST'])
+@app.route('/add_team', methods=['POST'])
 def add_team():
-    
 
     session['teams'][request.form['teamName']] = {}
-
 
     return render_template('main.html')
 
 
 if __name__ == '__main__':
     app.run()
-
 
 
 """
